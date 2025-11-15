931ba4cf16d8810e358fc8217124995a3228d7b4
User-Matheus- Trajano <matheustrajanofaetec@gmail.com>
2025-11-15 19:37:18 -0300

    Iniciando...

---
+++ AA2/includes/footer.php
@@ -0,0 +1,11 @@
+<footer>
+
+    <div class="div-footer"  aria-label="Rodapé do site">
+        <p>© 2025 - Criado por Matheus Trajano</p>
+    </div>
+    
+</footer>
+
+</body>
+</html>
+
---
+++ AA2/includes/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AA2</title>
+    <link rel="stylesheet" href="./assets/sty.css">
+    <script src="./assets/javas.js"></script>
+</head>
+
+<body>
+
+<header>
+    
+    <div class="div-header-desktop">
+        <div class="logotipo">
+          <h1>Introdução</h1>
+      </div>
+
+        <nav class="nav-header" aria-label="Menu principal do site">
+          <ul class="ul-header">
+          <li><a href="introducao.html" title="Ir para a página de introdução">Introdução</a></li>
+          <li><a href="sobre.html" title="Ir para a página sobre">Sobre</a></li>
+          <li><a href="objetivo.html" title="Ver metas">Metas</a></li>
+          <li><a href="novidades.html" title="Ler as novidades">Novidades</a></li>
+          <li><a href="contato.html" title="Entrar em contato">Contato</a></li>
+          </ul>
+        </nav>
+      </div>
+
+      <div class="div-header-mob">
+        
+        <nav class="vertical-menu">
+          <ul class="list-mob">
+            <li><a href="introducao.html" title="Ir para a página de introdução">Introdução</a></li>
+            <li><a href="sobre.html" title="Ir para a página sobre">Sobre</a></li>
+            <li><a href="objetivo.html" title="Ver metas">Metas</a></li>
+            <li><a href="novidades.html" title="Ler as novidades">Novidades</a></li>
+            <li><a href="contato.html" title="Entrar em contato">Contato</a></li>
+          </ul>
+        </nav>
+        
+      </div>
+
+</header>
